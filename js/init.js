f1bf26f83f5c85512d345411abea969b83c676fb
Tony Nguyen <tonyviet87@gmail.com>
2016-05-23 19:29:21 -0400

    Fixed portfolio carousel

--- js/init.js
+++ js/init.js
@@ -11,9 +11,22 @@
       $('.carousel').carousel();
     });
 
-    
+
   $(document).ready(function(){
     $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
   });
+
+
+  $(document).ready(function(){
+    $('.tooltipped').tooltip({delay: 50});
+  });
+        
+
+        
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+          
